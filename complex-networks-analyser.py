b98c42fc9d3f90a12aefb84f7acb98ba282a8807
Victor H. Heclis <vheeclis@gmail.com>
2017-09-06 20:06:23 -0300

    Minor changes
    Changed some networks because of project description changes

--- complex-networks-analyser.py
+++ complex-networks-analyser.py
@@ -1,4 +1,5 @@
 import networkx as nx
+from  plfit import plfit
 from matplotlib import pyplot as pl
 
 
@@ -6,10 +7,10 @@ from matplotlib import pyplot as pl
     Return a given statistic moment of a graph, where moment is the number of the moment
 """
 def stat_moment(graph, moment):
-	value = 0
-	for node in graph.nodes_iter():
-		value += graph.degree(node) ** moment
-	return value / graph.number_of_nodes()
+    value = 0
+    for node in graph.nodes_iter():
+        value += graph.degree(node) ** moment
+    return value / graph.number_of_nodes()
 
 
 """
@@ -17,21 +18,22 @@ def stat_moment(graph, moment):
     degree historgram (normalized), plotting it at the end
 """
 def process_graph(graph, name):
-	degree_histogram = nx.degree_histogram(graph)
-	number_of_nodes = nx.number_of_nodes(graph)
+    degree_histogram = nx.degree_histogram(graph)
+    number_of_nodes = nx.number_of_nodes(graph)
 
-	for index in range(len(degree_histogram)):
-		degree_histogram[index] = degree_histogram[index] / number_of_nodes
+    for index in range(len(degree_histogram)):
+        degree_histogram[index] = degree_histogram[index] / number_of_nodes
 
-	m1 = stat_moment(graph, 1)
-	m2 = stat_moment(graph, 2)
-	variance = m2 - (m1 ** 2)
-	print('M1: {:.5f}'.format(m1))
-	print('M2: {:.5f}'.format(m2))
-	print('Variance: {:.5f}'.format(variance))
-	pl.title('Degree distribution: '+ name)
-	pl.loglog(degree_histogram, 'b.')
-	pl.show()
+
+    m1 = stat_moment(graph, 1)
+    m2 = stat_moment(graph, 2)
+    variance = m2 - (m1 ** 2)
+    print('M1: {:.5f}'.format(m1))
+    print('M2: {:.5f}'.format(m2))
+    print('Variance: {:.5f}'.format(variance))
+    pl.title('Degree distribution: '+ name)
+    pl.loglog(degree_histogram, 'b.')
+    pl.show()
 
 """
     Return the greatest component of a given graph
@@ -44,7 +46,7 @@ def greatestComponent(graph):
     Initialize the graphs, where:
     pathFile: path to where is the file containing edge list 
     weighted: a bool variable, True if the graph is weighted and False if not
-    directed: a bool variable, True if the graph is directed and False if not
+    directed: a bool variable, True if the graph is directed and False if not (it's used if you want to turn the graph to undirected)
     jumps: number of lines that need to be jumped so we can reach the list of edges
 
 """
@@ -68,23 +70,22 @@ def initGraph(pathFile, weighted, directed, jumps):
 
 
 #Social Network
-GraphSHamsterster = initGraph('./connexions/social/hamsterster-friendships.txt', False, False, 1)
-GraphSMiserables = initGraph('./connexions/social/les-miserables.txt', True, False, 2)
+GraphSHamsterster = initGraph('./connexions/social/hamsterster.txt', False, False, 1)
 
 #Infrastructure Network
 GraphIPowerGrid = initGraph('./connexions/infrastructure/us-power-grid.txt', False, False, 2)
 
 #Transport Network
 GraphTEuroroad = initGraph('./connexions/transport/euroroad.txt', False, False, 2)
-GraphTAirTraffic = initGraph('./connexions/transport/air-traffic-control.txt', False, True, 1)
-
-#Biologic Network
-GraphBHumanProtein = initGraph('./connexions/biologic/human-protein-(figeys).txt', False, True, 1)
+GraphTUSAir = initGraph('./connexions/transport/us-airports.txt', True, False, 2) #directed is False because we don't want to change it to undirected
 
 #Calculating the Greates Component of each Graph
 GCHamsterster = greatestComponent(GraphSHamsterster)
-GCMiserables = greatestComponent(GraphSMiserables)
 GCPowerGrid = greatestComponent(GraphIPowerGrid)
 GCEuroroad = greatestComponent(GraphTEuroroad)
-GCAirTraffic = greatestComponent(GraphTAirTraffic)
-GCHumanProtein = greatestComponent(GraphBHumanProtein)
+GCUSAir = greatestComponent(GraphTUSAir)
+
+process_graph(GraphSHamsterster, 'Hamsterstes')
+process_graph(GraphIPowerGrid, 'Power Grid')
+process_graph(GraphTEuroroad, 'Euroroad')
+process_graph(GraphTUSAir, 'US Airport')
